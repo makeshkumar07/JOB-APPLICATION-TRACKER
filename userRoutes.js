e0bcb3c1e9511dea61b49563f2e54438b2ca158b
makesh kumar <makeshsaravanan2004@gmail.com>
2025-10-24 12:34:01 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,7 +0,0 @@
-// userRoutes.js
-const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
-const router = express.Router();
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-module.exports = router;
